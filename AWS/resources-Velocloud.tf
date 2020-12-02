1767967473c2b3c8252f5c9ddeb2bcd5bea87b54
Karim Benhamouche <kbenhamouche@kbenhamouch-a01.vmware.com>
2020-12-02 16:43:24 -0500

    v1.5

--- AWS/resources-Velocloud.tf
+++ AWS/resources-Velocloud.tf
@@ -11,24 +11,24 @@
 
 // GE1 definition - Management interface
 resource "aws_network_interface" "aws_velo-ge1" { 
-    subnet_id = var.aws_public_sn_cidr_block 
+    subnet_id = data.aws_subnet.aws_vcn-public-sn.id
     source_dest_check = false
-    security_groups = [var.aws_public_sg]
+    security_groups = [data.aws_security_group.aws_vcn-sg-wan.id]
 }
 
 // GE2 definition - WAN interface
 resource "aws_network_interface" "aws_velo-ge2" { 
-    subnet_id = var.aws_public_sn_cidr_block 
+    subnet_id = data.aws_subnet.aws_vcn-public-sn.id 
     source_dest_check = false
-    security_groups = [var.aws_public_sg]
+    security_groups = [data.aws_security_group.aws_vcn-sg-wan.id]
 }
 
 // GE3 definition - LAN interface
 resource "aws_network_interface" "aws_velo-ge3" { 
-    subnet_id = var.aws_private_sn_cidr_block 
+    subnet_id = data.aws_subnet.aws_vcn-private-sn.id 
     private_ips = [var.aws_private_ip]
     source_dest_check = false
-    security_groups = [var.aws_private_sg]
+    security_groups = [data.aws_security_group.aws_vcn-sg-lan.id]
 }
 
 // 2- Create SSH key pair
@@ -75,12 +75,10 @@ resource "aws_eip" "aws_velo-ge2-eip" {
     }
 }
 
-/*
 // 4- create static route to reach the private cloud management subnet via Velocloud CloudVPN
 resource "aws_route" "aws_branch_route" {
-    route_table_id = data.aws_vpc.aws_vcn-vpc.id.main_route_table_id
+    route_table_id = data.aws_vpc.aws_vcn-vpc.main_route_table_id
     depends_on = [aws_instance.aws_velo-instance, aws_network_interface.aws_velo-ge3]
     destination_cidr_block = "10.5.99.0/24"
     network_interface_id = aws_network_interface.aws_velo-ge3.id
 }
-*/
